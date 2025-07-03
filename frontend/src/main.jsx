c38810145c0e47eb6333c7133f3550c58dd77e2a
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2025-07-03 16:45:27 +0300

    Removed Tailwind directives from CSS, switched to pure CSS

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+  return <h1>Welcome to TableTime Foods</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
