2f5920541c0116fed40a1f8453b7436deb6dc5f5
nati-sahlu <natansahlu5@gmail.com>
2025-07-04 19:48:27 +0300

    connect all routes in app.js and test with curl

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,21 @@
-//base
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const registerRoute = require("./routes/register.route");
+const loginRoute = require("./routes/login.route");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use('/api', registerRoute);
+app.use('/api', loginRoute);
+
+const PORT= process.env.PORT;
+app.listen(PORT, () => {
+	console.log(` working on port ${PORT}`);
+});
