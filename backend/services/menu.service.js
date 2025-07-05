05ddbb055d5baa419469addb19f69e8369fef982
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2025-07-05 21:37:36 +0300

    feat: add menu service to fetch items by restaurant id

--- backend/services/menu.service.js
+++ backend/services/menu.service.js
@@ -1,5 +1,14 @@
+const db = require('../config/db.config');
+
+
 async function getMenuByRestaurantId(restaurantId) {
-  const sql = 'SELECT * FROM menu_items WHERE restaurant_id = ?';
-  const result = await db.query(sql, [restaurantId]);
-  return result;
+	const sql = 'SELECT * FROM menu_items WHERE restaurant_id = ?';
+	const result = await db.query(sql, [restaurantId]);
+	return result;
 }
+
+
+
+module.exports = {
+	getMenuByRestaurantId,
+};
