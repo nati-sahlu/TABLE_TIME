384a0276bf1a5be23103ade1938894daa1509dad
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2025-07-05 11:57:08 +0300

    put in restaurant routes

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
 const registerRoute = require("./routes/register.route");
 const loginRoute = require("./routes/login.route");
+const restaurantRoutes = require('./routes/restaurant.route');
+const menuRoutes = require('./routes/menu.route');
 
 const app = express();
 
@@ -14,6 +15,9 @@ app.use(express.json());
 // routes
 app.use('/api', registerRoute);
 app.use('/api', loginRoute);
+app.use('/api/restaurants', restaurantRoutes);
+app.use('/api', menuRoutes);
+
 
 const PORT= process.env.PORT;
 app.listen(PORT, () => {
