ef0932baac91f889f38ac12e2dfbc6f73941f452
nati-sahlu <natansahlu5@gmail.com>
2025-07-05 23:52:16 +0300

    added post route for adding menu item

--- backend/routes/menu.route.js
+++ backend/routes/menu.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {fetchMenu} = require('../controllers/menu.controller');
+const {fetchMenu, addMenuItem} = require('../controllers/menu.controller');
 
 
 router.get('/restaurants/:id/menu', fetchMenu);
+router.post('/restaurants/:id/menu', addMenuItem);
 
 module.exports = router;
