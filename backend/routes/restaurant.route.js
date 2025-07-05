9bbbd7cb9bb887c825607a40f8b64d7270b651e4
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2025-07-05 19:53:57 +0300

    connected restaurant route to app router

--- backend/routes/restaurant.route.js
+++ backend/routes/restaurant.route.js
@@ -1,9 +1,8 @@
 
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/restaurant.controller');
+const {searchRestaurants} = require('../controllers/restaurant.controller');
 
-router.get('/search', controller);
-router.get('/restaurants/:id/details', controller.fetchRestaurantWithMenu);
+router.get('/restaurants', searchRestaurants);
 
 module.exports = router;
