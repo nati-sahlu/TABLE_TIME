a1922e9c48e503ba50cde5f8a5434876a9c24f74
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2025-07-09 23:44:53 +0300

    feat: add DELETE route for menu item deletion
    - Registered new DELETE /menu/:id route
    - Imported deleteMenuItem from menu.controller

--- backend/routes/menu.route.js
+++ backend/routes/menu.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {fetchMenu, addMenuItem} = require('../controllers/menu.controller');
+const {fetchMenu, addMenuItem, deleteMenuItem} = require('../controllers/menu.controller');
 
 
 router.get('/restaurants/:id/menu', fetchMenu);
 router.post('/restaurants/:id/menu', addMenuItem);
+router.delete("/menu/:id", deleteMenuItem);
 
 module.exports = router;
